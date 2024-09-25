2b4df9881540fcbafc5300a80a2c3b43927d6528
urjabahad <127567639+urjabahad@users.noreply.github.com>
2024-09-25 09:57:05 +0530

    Time: 2 ms (76.81%), Space: 53.1 MB (40.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //key,value=>number and frequency
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i : nums)
-        {
-            if(map.containsKey(i))
-            map.put(i,map.get(i)+1);
-            else
-            map.put(i,1);
+        //MOORES VOTING ELEMENT
+        int count=0;
+        int candidate=0;
+        for(int n : nums){
+            if(count==0){
+                candidate=n;
+            }
+            if(n==candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-        for(Map.Entry<Integer,Integer> entry : map.entrySet())
-        {
-            if(entry.getValue()>(nums.length/2))
-            return entry.getKey();
-        }
-        return 0;
+        //here since we know a majority elemenet always we dont need to do second pass
+        return candidate;
     }
 }
